886654a3ba4d68a1aa9688a57641fe7b420c3df0
Juun Pak <june.mintlatte@gmail.com>
2021-10-30 16:24:11 +0900

    Implement function palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 
 module.exports = palindrome;
+
+/*
+function palindrome(str) {
+  const reversedStr = str
+    .split('')
+    .reduce((reversed, character) => character + reversed, '');
+  return str === reversedStr;
+}
+*/
