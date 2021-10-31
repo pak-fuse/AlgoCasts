279cb68b58ee06d5d29cdb8048d98392dac1a6af
Juun Pak <june.mintlatte@gmail.com>
2021-10-31 21:08:54 +0900

    🎨 Implement function fizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,42 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
+
+/*
+function fizzBuzz(n) {
+  let numArr = [];
+  let i = 0;
+
+  do {
+    i = i + 1;
+    numArr.push(i);
+  } while (i < n);
+
+  numArr.forEach(num => {
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (num % 3 === 0) {
+      console.log('fizz');
+    } else if (num % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(num);
+    }
+  });
+}
+*/
