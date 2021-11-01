f078155c11c53606e6862ced9a71e5ffeda3fbde
Juun Pak <june.mintlatte@gmail.com>
2021-11-01 21:13:00 +0900

    🎨 Implement function steps include in recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Solution #2 : Recursive function
+function steps(n, row = 0, stair = '') {
+  if (n === row) return;
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#' : ' ';
+
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
+
+/*
+// My Answer
+function steps(n) {
+  let result = '';
+
+  for (let i = 0; i < n; i++) {
+    result += '#';
+    console.log(result.padEnd(n, ' '));
+  }
+}
+
+// Solution #1
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+
+    for (let col = 0; col < n; col++) {
+      stair = col <= row ? stair + '#' : stair + ' ';
+    }
+
+    console.log(stair);
+  }
+}
+*/
